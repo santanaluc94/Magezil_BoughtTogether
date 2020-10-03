1f62461c5ade55e24c4b6627124e3404a53001f9
Lucas Santana <lsantana@itelios.com>
2020-10-03 20:32:03 -0300

    Fixing button all selected to cart

--- view/frontend/web/js/carouselBoughtTogether.js
+++ view/frontend/web/js/carouselBoughtTogether.js
@@ -16,7 +16,7 @@ define([
 
     // Select all products
     let selectAllLink = document.getElementById('select_all_bought_together');
-    let similarProductsCheckbox = document.getElementsByClassName('block bought-together')[0].getElementsByClassName('checkbox bought-together');
+    let similarProductsCheckbox = document.getElementsByClassName('block bought-together')[0].getElementsByClassName('checkbox-bought-together');
     let button = false;
 
     selectAllLink.onclick = function () {
@@ -24,14 +24,14 @@ define([
             button = false;
             Array.prototype.forEach.call(similarProductsCheckbox, function (item) {
                 item.checked = false;
-                document.getElementById('button_select_all_bought_together').innerHTML = $.mage.__("select all");
+                document.getElementById('select_all_bought_together').innerHTML = $.mage.__("select all");
             });
         } else {
             button = true;
             Array.prototype.forEach.call(similarProductsCheckbox, function (item) {
                 item.checked = true;
             });
-            document.getElementById('button_select_all_bought_together').innerHTML = $.mage.__("unselect all");
+            document.getElementById('select_all_bought_together').innerHTML = $.mage.__("unselect all");
         }
     }
 
